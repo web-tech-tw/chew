d0e7f619c45c74bdbe11b40421fb0a7a16629958
不知火 Shiranui <supersonic@livemail.tw>
2024-11-09 21:37:37 +0800

    feat: add redirectLogin

--- src/components/AppHeaderMenuData.js
+++ src/components/AppHeaderMenuData.js
@@ -1,12 +1,13 @@
+import {
+  redirectLogin,
+} from "../plugins/profile";
+
 export const title = "Chew 口香糖";
 export const subtitle = "文字片段分享";
 
 export const isSaraEnabled = true;
 export const onClickSara = () => {
-  const {
-    VITE_SARA_INTE_HOST: saraInteHost,
-  } = import.meta.env;
-  location.assign(saraInteHost);
+  redirectLogin();
 };
 
 export const menuItems = [];
