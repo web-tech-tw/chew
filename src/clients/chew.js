c1c3404c1db2f181d96589d4fa1874ad8ab45016
不知火 Shiranui <supersonic@livemail.tw>
2024-11-09 15:33:39 +0800

    refactor(chew.js): rename useSaraToken to readSaraToken for clarity

--- src/clients/chew.js
+++ src/clients/chew.js
@@ -5,14 +5,14 @@ const {
     VITE_SARA_TOKEN_NAME: tokenName,
 } = import.meta.env;
 
-const useSaraToken = (request) => {
+const readSaraToken = (request) => {
     const tokenValue = localStorage.getItem(tokenName);
     if (!tokenValue) return;
     request.headers.set("authorization", `SARA ${tokenValue}`);
 };
 
 export const useClient = (withAuth = true) => {
-    const beforeRequest = withAuth ? [useSaraToken] : [];
+    const beforeRequest = withAuth ? [readSaraToken] : [];
     return ky.create({
         prefixUrl: baseUrl,
         hooks: { beforeRequest },
