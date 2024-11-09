1b1c90de8c1d8ae111310794c6b44f163786922f
不知火 Shiranui <supersonic@livemail.tw>
2024-11-09 21:59:52 +0800

    refactor(appheadermenudata.js): handle login state

--- src/components/AppHeaderMenuData.js
+++ src/components/AppHeaderMenuData.js
@@ -6,8 +6,9 @@ export const title = "Chew 口香糖";
 export const subtitle = "文字片段分享";
 
 export const isSaraEnabled = true;
-export const onClickSara = () => {
-  redirectLogin();
+export const onClickSara = (profile) => {
+  const isLoggedIn = !!profile;
+  redirectLogin(!isLoggedIn);
 };
 
 export const menuItems = [];
